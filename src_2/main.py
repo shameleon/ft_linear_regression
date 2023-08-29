62ba808abfc8faaadfdf354c786c985d01d2df16
jmouaike <john.mouaikel@gmail.com>
2023-08-29 17:40:26 +0200

    read theta file

---
+++ src_2/main.py
@@ -0,0 +1,12 @@
+
+def main() -> None:
+    """ """
+    print("Hello World")
+    pass
+
+
+if __name__ == "__main__":
+    """training model then predicting"""
+    main()
+
+# https://www.geeksforgeeks.org/gradient-descent-in-linear-regression/
---
+++ src_2/train_model.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class LinearRegressionModel:
+    """ """
+    def __init__(self):
+        """ """
+        self.url = 'https://cdn.intra.42.fr/document/document/18562/data.csv'
+        self.data = pd.read_csv(self.url, sep=",", usecols=['km', 'price'])
+        # self.data.sort_values(by=['km'], inplace=True, ascending=True)
+        self.x = self.data['km'].to_numpy
+        self.y = self.data['price'].to_numpy
+        self.arr = self.data[['km', 'price']].to_numpy()
+        self.origin = 9000.0    # set to zero
+        self.slope = -0.025     # set to zero
+        return None
+
+    def plot_data(self):
+        """ """
+        xn = np.linspace(min(self.arr[0]), max(self.arr[0]), 100).astype(int)
+        yn = xn * self.slope + self.origin
+        plt.scatter(self.data["km"], self.data["price"], c='orange')
+        plt.plot(xn, yn)
+        plt.show()
+        return None
+
+    def get_coeffs(self):
+        coeffs = {"origin": self.origin, "slope": self.slope}
+        return coeffs
+
+    def __str__(self):
+        """https://www.scaler.com/topics/python-str/"""
+        return f'\x1b[6;30;60m Training [ok]\n model :\
+            {self.origin} + m * {self.slope}.\x1b[0m'
+
+
+"""
+https://www.geeksforgeeks.org/gradient-descent-in-linear-regression/
+
+https://docs.python.org/3/tutorial/errors.html
+
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+        f.close()
+
+"""
