947a0ab71cdd7edf6d469cb3563e0aa11f058906
John Mouaikel <john.mouaikel@gamil.com>
2023-09-03 21:18:30 +0200

    exceptions to prediction

--- src_3/predict.py
+++ src_3/predict.py
@@ -20,6 +20,8 @@ class PredictPriceFromModel():
         self.model = file
         self.theta = np.zeros(2)
         self.upload_model()
+        self.mileage = 0
+        self.price = self
 
     def upload_model(self):
         try:
@@ -45,23 +47,70 @@ class PredictPriceFromModel():
 
     def ask_for_mileage(self):
         """ """
-        in_str = input('\x1b[6;30;43m'
+        try:
+            in_str = input('\x1b[6;30;43m'
                     + 'Please enter a car mileage:'
                     + '\x1b[0m\n')
-        
+            self.mileage = float(in_str)
+            if self.mileage < 0 or self.mileage > 1E6:
+                raise InvalidMileageRangeError(self.mileage)
+            self.price = self.predict_price(self.mileage) 
+            if self.price < 0:
+                raise NegativePredictedPriceError()
+        except (RecursionError, RuntimeError, TypeError, ValueError):
+            print("Error : input is not valid")
+        except (EOFError):
+            print("Error : CTRL + D is not a mileage, lol")
+        except (NegativePredictedPriceError):
+            self.price = 0
+            print("Error: predicted price is out of range")
+        except (InvalidMileageRangeError):
+            print("Error: mileage is out of range")
+        else:
+            print(self)
+
+    def __str__(self):
+        return f'mileage = {self.mileage} km    predicted price = {self.price}'
+    
+class InvalidMileageRangeError(Exception):
+    """ Exception raised for errors in the input Mileage"""
+    def __init__(self, mileage, message="Invalid mileage : out of range"):
+        self.mileage = mileage
+        self.message = message
+        super().__init__(self.message)
+
+class NegativePredictedPriceError(Exception):
+    """ Exception raised for errors in the input Mileage"""
+
+    def __init__(self, message="Invalid price : out of range"):
+        self.message = message
+        super().__init__(self.message)
+
+
 def intro():
     print(f'\n{COL_BLUWHI}----------- PREDICT A CAR PRICE -----------{COL_RESET}\n\n')
-    
-def ask_for_mileage(self):
-        """ """
+
+def test_predict_class(model:PredictPriceFromModel):
+    mileages = [0, 25000, 100000, 200000, 35236, 89465.8, -5000, 4597866]
+    for mileage in mileages:
+        pred_price = model.predict_price(mileage)
+        print('mileage = {} km    predicted price = {:.2f} $'.format(mileage, pred_price))
+
 
 def main():
     intro()
-    my_model = PredictPriceFromModel("./gradient_descent_model/theta.csv")
-    mileages = [0, 25000, 100000, 200000, 35236, 89465.8, -5000, 4597866]
-    for mileage in mileages:
-        pred_price = my_model.predict_price(mileage)
-        print('mileage = {} km    predicted price = {:.2f} $'.format(mileage, pred_price))
+    model_prediction = PredictPriceFromModel("./gradient_descent_model/theta.csv")
+    # test_predict_class(model_prediction)
+    continue_loop = True
+    while(continue_loop):
+        model_prediction.ask_for_mileage()
+        # print(model_prediction)
+        in_str = input('\r\x1b[1;33;40m'
+                    + 'press nothing or any + [enter] to continue \n  [q + enter] to quit]'
+                    + '\x1b[0m\n\r')
+        if (in_str in ["q", "Q", "exit", "quit"]):
+            continue_loop = False
+
 
 if __name__ == "__main__":
     main()
